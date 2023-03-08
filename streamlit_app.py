5eab9e33abc985278fb1c184d00106486c33c373
Srinath Murali <65416747+srinath-96@users.noreply.github.com>
2023-03-08 22:29:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,19 @@ import streamlit as st
 import requests
 import json
 import os
-
+import pyaudio
+import base64
 # title and favicon ------------------------------------------------------------
 
 st.set_page_config(
-    page_title="Speech-to-Text Transcription App", page_icon="👄", layout="wide"
+    page_title="Speech-to-Text Transcription App", page_icon="-o-", layout="wide"
 )
 
 # App layout width -------------------------------------------------
 
 
 def _max_width_():
-    max_width_str = f"max-width: 1200px;"
+    max_width_str = f"max-width: 2400px;"
     st.markdown(
         f"""
     <style>
@@ -35,7 +36,7 @@ _max_width_()
 c30, c31, c32 = st.columns([2.5, 1, 3])
 
 with c30:
-    st.image(r"C:\Users\Dell\Desktop\speech-to-text-streamlit-app-main\logo.png", width=350)
+    st.image(r"C:\Users\Dell\Desktop\speech-to-text-streamlit-app-main\logo.png", width=700)
     st.header("")
 
 with c32:
@@ -54,7 +55,7 @@ with c32:
 st.text("")
 st.markdown(
     f"""
-                    The speech to text recognition is done via the [Facebook's Wav2Vec2 model.](https://huggingface.co/facebook/wav2vec2-large-960h)
+                    The speech to text recognition is done via the (https://huggingface.co/theainerd/Wav2Vec2-large-xlsr-hindi)
                     """
 )
 st.text("")
@@ -66,7 +67,8 @@ st.text("")
 
 def main():
     pages = {
-        "🤗 Full mode (with your API key)": API_key,
+        "Wav file": API_key,
+        "Real Time":demo
     }
 
     if "page" not in st.session_state:
@@ -74,6 +76,8 @@ def main():
             {
                 # Default page
                 "page": "Home",
+                "run" : False ,
+                "text" : "Listening to your Audio..."
             }
         )
 
@@ -84,6 +88,118 @@ def main():
 
 
 
+# endregion main
+
+# Free mode -------------------------------------------------
+def start_listening():
+	st.session_state['run'] = True
+
+def stop_listening():
+	st.session_state['run'] = False
+
+def demo():
+
+    c1, c2, c3 = st.columns([1, 4, 1])
+    with c2:
+        
+        with st.form(key="my_form"):
+            
+            text_input = st.text_input("Enter your HuggingFace API key")
+            FRAMES_PER_BUFFER = int(st.sidebar.text_input('Frames per buffer', 3200))
+            FORMAT = pyaudio.paInt16
+            CHANNELS = 1
+            RATE = int(st.sidebar.text_input('Rate', 16000))
+            p = pyaudio.PyAudio()
+
+            # Open an audio stream with above parameter settings
+            f = p.open(
+               format=FORMAT,
+               channels=CHANNELS,
+               rate=RATE,
+               input=True,
+               frames_per_buffer=FRAMES_PER_BUFFER
+            )
+
+
+            c2.button('Start', on_click=start_listening)
+            c2.button('Stop', on_click=stop_listening)
+            
+            st.info(
+                f"""
+                        👆 record audio: 
+                        """
+            )
+    
+            submit_button = st.form_submit_button(label="Transcribe")
+    
+    if not submit_button:
+    
+        st.stop()
+    
+    else:
+    
+        try:
+    
+            if f is not None:
+                data = f.read(FRAMES_PER_BUFFER)
+                bytes_data = base64.b64encode(data)
+                # getsize
+                getsize=20
+                if getsize < 30:  # File more than 30MB
+
+    
+                    api_token = text_input
+    
+                    headers = {"Authorization": f"Bearer {api_token}"}
+                    API_URL = "https://api-inference.huggingface.co/models/theainerd/Wav2Vec2-large-xlsr-hindi"
+    
+                    def query(data):
+                        response = requests.request(
+                            "POST", API_URL, headers=headers, data=data
+                        )
+                        return json.loads(response.content.decode("utf-8"))
+    
+                    data = query(bytes_data)
+    
+                    # data = query(bytes_data)
+    
+                    values_view = data.values()
+                    value_iterator = iter(values_view)
+                    text_value = next(value_iterator)
+                    text_value = text_value.lower()
+                    st.success(text_value)
+    
+                    c0, c1 = st.columns([2, 2])
+    
+                    with c0:
+                        st.download_button(
+                            "Download the transcription",
+                            text_value,
+                            file_name=None,
+                            mime=None,
+                            key=None,
+                            help=None,
+                            on_click=None,
+                            args=None,
+                            kwargs=None,
+                        )
+    
+                else:
+                    st.error(
+                        "This app is still in early development so we've maxed out the file size limit. Please try again with a smaller file."
+                    )
+                    st.stop()
+    
+            else:
+                path_in = None
+                st.info(
+                    f"""
+                        👆 Upload a .wav file. Or try a sample: [Wav sample 01](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/Welcome.wav?raw=true) | [Wav sample 02](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/The_National_Park.wav?raw=true)
+                        """
+                )
+    
+        except ValueError:
+            "ValueError"
 
 
 
@@ -103,7 +219,7 @@ def API_key():
 
             st.info(
                 f"""
-                        👆 Upload a .wav file. Or try a sample: [Wav sample 01](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/Welcome.wav?raw=true) | [Wav sample 02](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/The_National_Park.wav?raw=true)
+                        👆 Upload a .wav file. Or record audio: 
                         """
             )
 
@@ -196,9 +312,8 @@ with st.expander("ℹ️ - About this app", expanded=False):
     st.write(
         """     
 
--   The Free mode is limited to 2MB of the audio file. You can use your API key to transcribe audio files of up to 30MB!
--   The tool is still in Beta; don't hesitate to contact me at [![Gitter chat](https://badges.gitter.im/gitterHQ/gitter.png)](https://gitter.im/DataChaz/speech-to-text) if bugs or questions
--   This app is free. If it's helpful to you, you can [buy me a ☕](https://www.buymeacoffee.com/cwar05) to support my work
+-   Speech to text for hindi language.
+-   Currently works with wav files and the Live recorded audio is in progress
 	    """
     )
 
