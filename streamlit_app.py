f9ada63f70a586b918124b2b1677d0349d9af579
Srinath Murali <65416747+srinath-96@users.noreply.github.com>
2023-03-11 21:20:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ st.text("")
 def main():
     pages = {
         "Wav file": API_key,
-        "Real Time":demo
+       
     }
 
     if "page" not in st.session_state:
@@ -93,109 +93,6 @@ def start_listening():
 def stop_listening():
 	st.session_state['run'] = False
 
-def demo():
-
-    c1, c2, c3 = st.columns([1, 4, 1])
-    with c2:
-        
-        with st.form(key="my_form"):
-            
-            text_input = st.text_input("Enter your HuggingFace API key")
-            FRAMES_PER_BUFFER = int(st.sidebar.text_input('Frames per buffer', 3200))
-            FORMAT = pyaudio.paInt16
-            CHANNELS = 1
-            RATE = int(st.sidebar.text_input('Rate', 16000))
-            p = pyaudio.PyAudio()
-
-            # Open an audio stream with above parameter settings
-            f = p.open(
-               format=FORMAT,
-               channels=CHANNELS,
-               rate=RATE,
-               input=True,
-               frames_per_buffer=FRAMES_PER_BUFFER
-            )
-
-
-            c2.button('Start', on_click=start_listening)
-            c2.button('Stop', on_click=stop_listening)
-            
-            st.info(
-                f"""
-                        👆 record audio: 
-                        """
-            )
-    
-            submit_button = st.form_submit_button(label="Transcribe")
-    
-    if not submit_button:
-    
-        st.stop()
-    
-    else:
-    
-        try:
-    
-            if f is not None:
-                data = f.read(FRAMES_PER_BUFFER)
-                bytes_data = base64.b64encode(data)
-                # getsize
-                getsize=20
-                if getsize < 30:  # File more than 30MB
-
-    
-                    api_token = text_input
-    
-                    headers = {"Authorization": f"Bearer {api_token}"}
-                    API_URL = "https://api-inference.huggingface.co/models/theainerd/Wav2Vec2-large-xlsr-hindi"
-    
-                    def query(data):
-                        response = requests.request(
-                            "POST", API_URL, headers=headers, data=data
-                        )
-                        return json.loads(response.content.decode("utf-8"))
-    
-                    data = query(bytes_data)
-    
-                    # data = query(bytes_data)
-    
-                    values_view = data.values()
-                    value_iterator = iter(values_view)
-                    text_value = next(value_iterator)
-                    text_value = text_value.lower()
-                    st.success(text_value)
-    
-                    c0, c1 = st.columns([2, 2])
-    
-                    with c0:
-                        st.download_button(
-                            "Download the transcription",
-                            text_value,
-                            file_name=None,
-                            mime=None,
-                            key=None,
-                            help=None,
-                            on_click=None,
-                            args=None,
-                            kwargs=None,
-                        )
-    
-                else:
-                    st.error(
-                        "This app is still in early development so we've maxed out the file size limit. Please try again with a smaller file."
-                    )
-                    st.stop()
-    
-            else:
-                path_in = None
-                st.info(
-                    f"""
-                        👆 Upload a .wav file. Or try a sample: [Wav sample 01](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/Welcome.wav?raw=true) | [Wav sample 02](https://github.com/CharlyWargnier/CSVHub/blob/main/Wave_files_demos/The_National_Park.wav?raw=true)
-                        """
-                )
-    
-        except ValueError:
-            "ValueError"
 
 
 
